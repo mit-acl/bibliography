294e87cbf82c1cd699c02c61fa10a5241d4901d1
Andrew Fishberg <fishberg.dev@gmail.com>
2023-11-03 15:43:49 -0400

    added bibtex_test.py

---
+++ bibtex_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+import bibtexparser
+
+infile = 'ACL_Publications.bib'
+
+try:
+    with open(infile) as READ:
+        data = READ.read()
+    lib = bibtexparser.parse_string(data)
+    if len(lib.failed_blocks) > 0:
+        errors = True
+    else:
+        errors = False
+except Exception as e:
+    errors = False
+    print(type(e))
+    print(e)
+
+if errors:
+    print('something is wrong!')
+    sys.exit(1)
+else:
+    print('looks good!')
